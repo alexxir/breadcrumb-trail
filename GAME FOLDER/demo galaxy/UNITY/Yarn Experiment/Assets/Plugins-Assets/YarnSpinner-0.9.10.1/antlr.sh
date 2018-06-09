059d7464cbbf030bfafc5c8cd12572ca56612ff3
Mad Rovere <maddrovere@outlook.com>
2018-06-09 23:05:30 +1000

    Ok, here we go. Massive scene overall, yarn spinner integration. Still can't figure out why my json file isn't working.

--- GAME FOLDER/demo galaxy/UNITY/Yarn Experiment/Assets/Plugins-Assets/YarnSpinner-0.9.10.1/antlr.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-curl http://www.antlr.org/download/antlr-4.7-complete.jar -o $HOME/antlr4.jar
-export CLASSPATH=".:$HOME/antlr.jar:$CLASSPATH"
-alias antlr4='java -Xmx500M -cp "$HOME/antrl.jar:$CLASSPATH" org.antlr.v4.Tool'
---
+++ GAME FOLDER/demo galaxy/UNITY/Yarn Experiment/Assets/Scripts/YarnTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+/* public class YarnTest : Yarn.Unity.DialogueUIBehaviour
+{
+    public Text storyText;
+    public List<Button> optionButtons;
+
+    public Yarn.Unity.VariableStorageBehaviour variableStorage;
+
+    //This is a delegate
+    private Yarn.OptionChooser SetSelectedOption;
+
+    //DialogueUIBehaviour requires us to implement this 
+    public override IEnumerator RunCommand(Yarn.Command command)
+    {
+        yield break;
+    }
+} */
